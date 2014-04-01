acfd668851f91ea5e142364fb6ee5bae276decfc
Gianfranco Montoya <gms122@gmail.com>
2014-04-01 14:52:01 -0500

    plugin interaccion infhotel

--- system/expressionengine/third_party/infhotel/pi.infhotel.php
+++ system/expressionengine/third_party/infhotel/pi.infhotel.php
@@ -330,8 +330,8 @@ class Infhotel
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
         //curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-        //curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);     
-        //curl_setopt($ch, CURLOPT_URL,$url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);     
+        curl_setopt($ch, CURLOPT_URL,$url);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
             'Content-Type: Infhotel/json',                                                                                
             'Content-Length: ' . strlen($data_string))                                                                       
