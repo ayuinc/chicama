7135a377377b177cb8e493dd1da3cd3ea4f6ffe6
Gianfranco Montoya <gms122@gmail.com>
2014-04-01 17:36:22 -0500

    test app

--- system/expressionengine/third_party/infhotel/pi.infhotel.php
+++ system/expressionengine/third_party/infhotel/pi.infhotel.php
@@ -277,20 +277,20 @@ class Infhotel
         $hora_checkin = ee()->TMPL->fetch_param('hora_checkin');
         $hora_checkout = ee()->TMPL->fetch_param('hora_checkin');*/
         //$url = "http://es.magicseaweed.com/api/3XpBW72Em3wuAo7O0BYc17k582W308Ek/forecast/?spot_id=416&units=eu"; 
-        $data = array(  "FLlegada" => "05\/04\/14 12:00:00 a.m.",
-                        "FSalida" => "07\/04\/14 12:00:00 a.m.", 
+        $data = array(  "FLlegada" => "05/04/14 12:00:00 a.m.",
+                        "FSalida" => "07/04/14 12:00:00 a.m.", 
                         "HLlegada" => null,
                         "HSalida" => null,
                         "Habitaciones" => array(
                                             array("CantHab" => "1",
-                                                "FLlegadaReserva" => "05\/04\/14",
-                                                "FSalidaReserva" => "07\/04\/14",
+                                                "FLlegadaReserva" => "05/04/14",
+                                                "FSalidaReserva" => "07/04/14",
                                                 "NPrecio" => 120,  
                                                 "TCodigoHabitacion" => "110004",      
                                             ),
                                             array("CantHab" => "1",
-                                                "FLlegadaReserva" => "05\/04\/14",
-                                                "FSalidaReserva" => "07\/04\/14",
+                                                "FLlegadaReserva" => "05/04/14",
+                                                "FSalidaReserva" => "07/04/14",
                                                 "NPrecio" => 150,  
                                                 "TCodigoHabitacion" => "110005",      
                                             )
