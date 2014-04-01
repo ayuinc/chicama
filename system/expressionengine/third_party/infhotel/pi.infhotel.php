55709d41cd0957a101a6c98b229f6f9ef556fa98
Gianfranco Montoya <gms122@gmail.com>
2014-04-01 17:30:18 -0500

    test nuevo servicio infhotel

--- system/expressionengine/third_party/infhotel/pi.infhotel.php
+++ system/expressionengine/third_party/infhotel/pi.infhotel.php
@@ -329,12 +329,13 @@ class Infhotel
         $ch = curl_init($url);
         // Disable SSL verification
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);                                                                  
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string); 
+        curl_setopt($ch, CURLOPT_HEADER, false);                                                                 
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);     
         curl_setopt($ch, CURLOPT_URL,$url);
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
-            'Content-Type: Infhotel/json',                                                                                
+            'Content-Type: application/json',                                                                                
             'Content-Length: ' . strlen($data_string))                                                                       
         );
         $result = curl_exec($ch);
