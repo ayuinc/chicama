2837749eff7b5f372167c4ce9779d2bf39f3e9fb
Gianfranco Montoya <gms122@gmail.com>
2014-04-01 18:04:31 -0500

    test app

--- system/expressionengine/third_party/infhotel/pi.infhotel.php
+++ system/expressionengine/third_party/infhotel/pi.infhotel.php
@@ -329,11 +329,12 @@ class Infhotel
         // Disable SSL verification
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");                                                                     
         curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string); 
-        curl_setopt($ch, CURLOPT_HEADER, true);                                                                 
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);     
         curl_setopt($ch, CURLOPT_URL,$url);
-        curl_setopt($ch, CURLOPT_HTTPHEADER,array('Content-Type: application/json')); 
+        curl_setopt($ch, CURLOPT_HTTPHEADER,array(
+            'Content-Type: application/json',                                                                                
+            'Content-Length: ' . strlen($data_string))
+        ); 
         $result = curl_exec($ch);
         return $result;
         // Execute
