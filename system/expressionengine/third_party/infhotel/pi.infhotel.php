94f5da530b6e449cd1c8b17d8c83a7c8156c109b
Gianfranco Montoya <gms122@gmail.com>
2014-04-01 19:45:29 -0500

    test cURL

--- system/expressionengine/third_party/infhotel/pi.infhotel.php
+++ system/expressionengine/third_party/infhotel/pi.infhotel.php
@@ -277,8 +277,7 @@ class Infhotel
         $hora_checkin = ee()->TMPL->fetch_param('hora_checkin');
         $hora_checkout = ee()->TMPL->fetch_param('hora_checkin');*/
         //$url = "http://es.magicseaweed.com/api/3XpBW72Em3wuAo7O0BYc17k582W308Ek/forecast/?spot_id=416&units=eu"; 
-        $data = array(  array(
-                        "FLlegada" => "05/04/2014 12:00:00",
+        $data = array(  "FLlegada" => "05/04/2014 12:00:00",
                         "FSalida" => "07/04/2014 12:00:00", 
                         "HLlegada" => null,
                         "HSalida" => null,
@@ -321,7 +320,6 @@ class Infhotel
                                                 "TTipoTarjeta" => "02"    
                                             )
                                         )
-                        )
         );
 
         $data_string = json_encode($data, true);
