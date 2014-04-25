1f2165a14e5f9fd63378962c1f228f939cffa763
Gianfranco Montoya <gms122@gmail.com>
2014-04-25 10:54:29 -0500

    verificando complementos de php5

---
+++ system/expressionengine/third_party/vpost/pi.vpost.php
@@ -0,0 +1,70 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Memberlist Class
+ *
+ * @package     ExpressionEngine
+ * @category    Plugin
+ * @author      Gianfranco Montoya 
+ * @copyright   Copyright (c) 2014, Gianfranco Montoya 
+ * @link        http://www.ayuinc.com/
+ */
+
+$plugin_info = array(
+    'pi_name'         => 'Vpost',
+    'pi_version'      => '1.0',
+    'pi_author'       => 'Gianfranco Montoya ',
+    'pi_author_url'   => 'http://www.ayuinc.com/',
+    'pi_description'  => 'Permite realizar el pago en linea por medio de plugin de Vpost',
+    'pi_usage'        => Vpost::usage()
+);
+            
+class Vpost 
+{
+
+    var $return_data = "";
+    // --------------------------------------------------------------------
+
+        /**
+         * Memberlist
+         *
+         * This function returns a list of members
+         *
+         * @access  public
+         * @return  string
+         */
+    public function __construct(){
+    }
+
+    // --------------------------------------------------------------------
+
+    /**
+     * Usage
+     *
+     * This function describes how the plugin is used.
+     *
+     * @access  public
+     * @return  string
+     */
+    public static function usage()
+    {
+        ob_start();  ?>
+        The Memberlist Plugin simply outputs a
+        list of 15 members of your site.
+
+            {exp:vpost}
+
+        This is an incredibly simple Plugin.
+            <?php
+        $buffer = ob_get_contents();
+        ob_end_clean();
+        return $buffer;
+    }
+    public function phpinfo(){
+        
+        return phpinfo();
+    }
+    // END
+}
+/* End of file pi.vpost.php */
+/* Location: ./system/expressionengine/third_party/vpost/pi.vpost.php */
