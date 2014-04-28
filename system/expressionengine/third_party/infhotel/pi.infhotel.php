5b7a53be9cccb900cef55db12f0d5f05e9f5f422
Gianfranco Montoya <gms122@gmail.com>
2014-04-28 16:55:03 -0500

    commit decodificacion de la data

--- system/expressionengine/third_party/infhotel/pi.infhotel.php
+++ system/expressionengine/third_party/infhotel/pi.infhotel.php
@@ -277,10 +277,10 @@ class Infhotel
         $fecha_checkout = ee()->TMPL->fetch_param('fecha_checkout');
         $hora_checkin = ee()->TMPL->fetch_param('hora_checkin');
         $hora_checkout = ee()->TMPL->fetch_param('hora_checkin');*/
-        $data = array(  "FLlegada" => "2014-05-08 13:30:00.000",
-                        "FSalida" => "2014-05-18 15:45:00.000", 
-                        "HLlegada" => "2014-05-08 13:30:00.000",
-                        "HSalida" => "2014-05-18 15:45:00.000",
+        $data = array(  "FLlegada" => "2014-04-01 13:30:00.000",01042014
+                        "FSalida" => "2014-04-05 15:45:00.000", 
+                        "HLlegada" = "2014-04-01 13:30:00.000",
+                        "HSalida" => "2014-04-05 15:45:00.000",
                         "Habitaciones" => array(
                                             array("CantHab" => 1, 
                                                 "NPrecio" => 120,  
