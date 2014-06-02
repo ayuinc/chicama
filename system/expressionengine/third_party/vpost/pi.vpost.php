b73e3076593ddfd1d957cef1ee0d0e229172de76
Gianfranco Montoya <gms122@gmail.com>
2014-06-02 16:11:05 -0500

    test vpos

--- system/expressionengine/third_party/vpost/pi.vpost.php
+++ system/expressionengine/third_party/vpost/pi.vpost.php
@@ -170,68 +170,34 @@ class Vpost
        # Vector
        $VI = "0000000000000000";
 
-        $llavePrivadaCifrado = "-----BEGIN RSA PRIVATE KEY-----\n".
-        "MIICXAIBAAKBgQDr3OXAFjoBokuPfCWs1QScfz0Ejyn4JuEuMpZCRYGhn/1pWCN1\n".
-        "kHVXrfk4fDephOBRVWEJq03fKkAJAxNMTMSbcjyMdSvhP4o/n8wpPGdHT1FZq1rD\n".
-        "OOkKKRUwyuZm/h5+hR97ud3WTccQ7RRQHBCl2rTi7ofG56NddecjI7iw8QIDAQAB\n".
-        "AoGARfH1JDizLQbfF4b+eTmWq35ELxTxokiNYLDZxH9uSOr0MIhVw6h6U+0gyjJ0\n".
-        "I05nY1dJdp+ZcUPLR6Dk2Syu5MSCFhlfEkni8kb+pK1OlkwqZ4aF3mMKW1PbXB38\n".
-        "M/vBKiZyANOTHAtdp7HuOjsAFdGT6m3Xi0bEmpAUxES+YlECQQD5tbsFBwN217FQ\n".
-        "OzZSarosXh8QVsTO4D9LMNQGw188OdgZZD29i9JGgtzxfG4VQqW7TaRjWJK/JqXU\n".
-        "p+Fgi4olAkEA8c3fqG0bT7whPxokCJGV9xCHHDB2y5qCu8RoPAnRvTcRudpqvKJR\n".
-        "ZSXkqnDgz86VGovZei32yQENm7//GtQD3QJAZ9jrERksmKT1Ca/GVJosGL1/37Sm\n".
-        "Hn7l86g31SQ1G//WXiDxCD340fgkWI1t3oucDvwoLGSuiq5Q8tJqiVMevQJAGoY/\n".
-        "04TsawmzB+4BJ+N4dNqeR4xVa64uSxSXboaROFVTH3UDImAX16WZeMzbDFCX8IqI\n".
-        "t6tVF+WCraQZAsNhDQJBALz43SvNjptXnbaHKJCKWC0tGjtC0fDjm1Tsf8jfka8o\n".
-        "hUzYmghZ2EUMA+zT18bWVBMCJ5fSD/vjBTxoF0MMmuk=\n".
-        "-----END RSA PRIVATE KEY-----\n";
+        $llaveVPOSCryptoPub = "-----BEGIN PUBLIC KEY-----\n".
+        "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDTJt+hUZiShEKFfs7DShsXCkoq\n".
+        "TEjv0SFkTM04qHyHFU90Da8Ep1F0gI2SFpCkLmQtsXKOrLrQTF0100dL/gDQlLt0\n".
+        "Ut8kM/PRLEM5thMPqtPq6G1GTjqmcsPzUUL18+tYwN3xFi4XBog4Hdv0ml1SRkVO\n".
+        "DRr1jPeilfsiFwiO8wIDAQAB\n".
+        "-----END PUBLIC KEY-----";
 
-        $llavePublicaFirma = "-----BEGIN PUBLIC KEY-----\n".
-        "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCw9S8vnGIW04tG4N98f0HXoMJl\n".
-        "B6K9v2iJZlsFYNtP6Xv6Ax5dLEBzym/edPj89lraAxzmZUVPzPVoLlT0gSZMjlPP\n".
-        "PfTyQp4QRWkME9AtT71q5gw5lISCW0X8bVWqwksJ515Va0LMjSohf2V+azVw2QBT\n".
-        "S3IofH5DUfPOjSAfXwIDAQAB\n".
-        "-----END PUBLIC KEY-----" ; 
-
-         $arrayIn['IDACQUIRER'] =$IDACQUIRER;
-         $arrayIn['IDCOMMERCE'] = $IDCOMMERCE;
-         $arrayIn['XMLRES'] = $XMLRES;
-         $arrayIn['DIGITALSIGN'] = $DIGITALSIGN;
-         $arrayIn['SESSIONKEY'] = $SESSIONKEY;
-
-         $arrayOut = '';
-
-        //valor del vector de inicializacion
-         $VI = "0000000000000000";
-
-          if(VPOSResponse($arrayIn,$arrayOut,$llavePublicaFirma,$llavePrivadaCifrado,$VI)){
-
-            echo ('OK');
-          //La salida esta en $arrayOut con todos los parámetros decifrados devueltos por  el VPOS
-            $resultadoAutorizacion = $arrayOut['authorizationResult'];
-            $codigoAutorizacion = $arrayOut['authorizationCode'];
-            $codigoError = $arrayOut['errorCode'];
-            $errormensaje = $arrayOut['errorMessage'];
-
-            echo ('<br>');
-            echo $resultadoAutorizacion;
-            echo ('<br>');
-            echo $codigoAutorizacion;
-            echo ('<br>');
-            echo $codigoError;
-            echo ('<br>');
-            echo $errormensaje;
-            echo ('<br>');
-
-          // se deben revisar luego los demas campos de acuerdo a lo indicado en la guia de integracion
-
-          }else{
-
-          //Puede haber un problema de mala configuración de las llaves,
-          //vector deinicializacion o el VPOS no ha enviado valores
-          //correctos
-              echo "<br> Respuesta Invalida";
-          }
+        $llavePrivadaFirmaComercio = "-----BEGIN RSA PRIVATE KEY-----\n".
+        "MIICWwIBAAKBgQCw9S8vnGIW04tG4N98f0HXoMJlB6K9v2iJZlsFYNtP6Xv6Ax5d\n".
+        "LEBzym/edPj89lraAxzmZUVPzPVoLlT0gSZMjlPPPfTyQp4QRWkME9AtT71q5gw5\n".
+        "lISCW0X8bVWqwksJ515Va0LMjSohf2V+azVw2QBTS3IofH5DUfPOjSAfXwIDAQAB\n".
+        "AoGAfb060jHk4SNgC/Ut2GD0gCuS9gb+9KVVuowokSHJtHbLyVL9+GbBRYXLB99G\n".
+        "LTlARTmBB5VeMt4IYwbJBxPeCcj49sHwDpPzltSZSN6kp11f8o28Tj/UjjGnuMZY\n".
+        "l4tSGlUWErojWe8/61YMoSK7700yOxOHTtZIlzuJ1a7OgcECQQDhtF2R1Zx9oRtb\n".
+        "Bt7RQqyiCZV2X/8dEJgoLV+ruCDBVcnpM4Irbiw9Bio+iHpo8gZA0FCfdGUMjF0A\n".
+        "i+LmNR0dAkEAyLXJZNbmN6wyTwzT+838guo8W+6/CiZ6rfCqk4Sooqibph6k5QZu\n".
+        "g84mMJdm4F8zIZGNvqtKVUwGk149DZXRqwJAXrqGsxiGb6vYgWHmzsk/D1saYe50\n".
+        "ckxusB4rEzVaegp5jejSb0v0QUB/JnO9fKJnwXgs/l+psZSPzBTqDFiqeQJAGMK8\n".
+        "brFIQ3P93NyzRiw6S5hEC/9fGAx5M/4tvPcvqqlsUkkThKGPfrku4u26plF4SFrh\n".
+        "hrUw/WbcpM+KbqOd8wJAeOJbi6H9y9VonyQYJM7yXwhNeAvlKTYEyYPeW2O7oitg\n".
+        "1Nxmog30epbOchoAmCAr2TPzbpentnvCO1hKbO3Jkw==\n".
+        "-----END RSA PRIVATE KEY-----"
+        if (VPOSSend($array_send,$arrayOut,$llaveVPOSCryptoPub,$llavePrivadaFirmaComercio,$VI)) {
+        }else{
+            $msj = "Hay un problema con el conector de pago"; //puede haber un problema de mala configuraciÃ³n de las llaves, vector de
+            //inicializacion o el VPOS no ha enviado valores correctos
+        }
+        return $idorden;
     }
 
     public function test_envio(){
@@ -266,69 +232,34 @@ class Vpost
        # Vector
        $VI = "0000000000000000";
 
-        $llavePrivadaCifrado = "-----BEGIN RSA PRIVATE KEY-----\n".
-        "MIICXAIBAAKBgQDr3OXAFjoBokuPfCWs1QScfz0Ejyn4JuEuMpZCRYGhn/1pWCN1\n".
-        "kHVXrfk4fDephOBRVWEJq03fKkAJAxNMTMSbcjyMdSvhP4o/n8wpPGdHT1FZq1rD\n".
-        "OOkKKRUwyuZm/h5+hR97ud3WTccQ7RRQHBCl2rTi7ofG56NddecjI7iw8QIDAQAB\n".
-        "AoGARfH1JDizLQbfF4b+eTmWq35ELxTxokiNYLDZxH9uSOr0MIhVw6h6U+0gyjJ0\n".
-        "I05nY1dJdp+ZcUPLR6Dk2Syu5MSCFhlfEkni8kb+pK1OlkwqZ4aF3mMKW1PbXB38\n".
-        "M/vBKiZyANOTHAtdp7HuOjsAFdGT6m3Xi0bEmpAUxES+YlECQQD5tbsFBwN217FQ\n".
-        "OzZSarosXh8QVsTO4D9LMNQGw188OdgZZD29i9JGgtzxfG4VQqW7TaRjWJK/JqXU\n".
-        "p+Fgi4olAkEA8c3fqG0bT7whPxokCJGV9xCHHDB2y5qCu8RoPAnRvTcRudpqvKJR\n".
-        "ZSXkqnDgz86VGovZei32yQENm7//GtQD3QJAZ9jrERksmKT1Ca/GVJosGL1/37Sm\n".
-        "Hn7l86g31SQ1G//WXiDxCD340fgkWI1t3oucDvwoLGSuiq5Q8tJqiVMevQJAGoY/\n".
-        "04TsawmzB+4BJ+N4dNqeR4xVa64uSxSXboaROFVTH3UDImAX16WZeMzbDFCX8IqI\n".
-        "t6tVF+WCraQZAsNhDQJBALz43SvNjptXnbaHKJCKWC0tGjtC0fDjm1Tsf8jfka8o\n".
-        "hUzYmghZ2EUMA+zT18bWVBMCJ5fSD/vjBTxoF0MMmuk=\n".
-        "-----END RSA PRIVATE KEY-----\n";
+       $llaveVPOSCryptoPub = "-----BEGIN PUBLIC KEY-----\n".
+        "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDTJt+hUZiShEKFfs7DShsXCkoq\n".
+        "TEjv0SFkTM04qHyHFU90Da8Ep1F0gI2SFpCkLmQtsXKOrLrQTF0100dL/gDQlLt0\n".
+        "Ut8kM/PRLEM5thMPqtPq6G1GTjqmcsPzUUL18+tYwN3xFi4XBog4Hdv0ml1SRkVO\n".
+        "DRr1jPeilfsiFwiO8wIDAQAB\n".
+        "-----END PUBLIC KEY-----";
 
-        $llavePublicaFirma = "-----BEGIN PUBLIC KEY-----\n".
-        "MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCw9S8vnGIW04tG4N98f0HXoMJl\n".
-        "B6K9v2iJZlsFYNtP6Xv6Ax5dLEBzym/edPj89lraAxzmZUVPzPVoLlT0gSZMjlPP\n".
-        "PfTyQp4QRWkME9AtT71q5gw5lISCW0X8bVWqwksJ515Va0LMjSohf2V+azVw2QBT\n".
-        "S3IofH5DUfPOjSAfXwIDAQAB\n".
-        "-----END PUBLIC KEY-----" ; 
-
-         $arrayIn['IDACQUIRER'] =144;
-         $arrayIn['IDCOMMERCE'] = 6573;
-         $arrayIn['XMLRES'] = "";
-         $arrayIn['DIGITALSIGN'] = "";
-         $arrayIn['SESSIONKEY'] = "";
-
-         $arrayOut = '';
-
-        //valor del vector de inicializacion
-         $VI = "0000000000000000";
-
-          if(VPOSResponse($arrayIn,$arrayOut,$llavePublicaFirma,$llavePrivadaCifrado,$VI)){
-
-            echo ('OK');
-          //La salida esta en $arrayOut con todos los parámetros decifrados devueltos por  el VPOS
-            $resultadoAutorizacion = $arrayOut['authorizationResult'];
-            $codigoAutorizacion = $arrayOut['authorizationCode'];
-            $codigoError = $arrayOut['errorCode'];
-            $errormensaje = $arrayOut['errorMessage'];
-
-            echo ('<br>');
-            echo $resultadoAutorizacion;
-            echo ('<br>');
-            echo $codigoAutorizacion;
-            echo ('<br>');
-            echo $codigoError;
-            echo ('<br>');
-            echo $errormensaje;
-            echo ('<br>');
-
-          // se deben revisar luego los demas campos de acuerdo a lo indicado en la guia de integracion
-
-          }else{
-
-          //Puede haber un problema de mala configuración de las llaves,
-          //vector deinicializacion o el VPOS no ha enviado valores
-          //correctos
-              echo "<br> Respuesta Invalida";
-          }
-    }
+        $llavePrivadaFirmaComercio = "-----BEGIN RSA PRIVATE KEY-----\n".
+        "MIICWwIBAAKBgQCw9S8vnGIW04tG4N98f0HXoMJlB6K9v2iJZlsFYNtP6Xv6Ax5d\n".
+        "LEBzym/edPj89lraAxzmZUVPzPVoLlT0gSZMjlPPPfTyQp4QRWkME9AtT71q5gw5\n".
+        "lISCW0X8bVWqwksJ515Va0LMjSohf2V+azVw2QBTS3IofH5DUfPOjSAfXwIDAQAB\n".
+        "AoGAfb060jHk4SNgC/Ut2GD0gCuS9gb+9KVVuowokSHJtHbLyVL9+GbBRYXLB99G\n".
+        "LTlARTmBB5VeMt4IYwbJBxPeCcj49sHwDpPzltSZSN6kp11f8o28Tj/UjjGnuMZY\n".
+        "l4tSGlUWErojWe8/61YMoSK7700yOxOHTtZIlzuJ1a7OgcECQQDhtF2R1Zx9oRtb\n".
+        "Bt7RQqyiCZV2X/8dEJgoLV+ruCDBVcnpM4Irbiw9Bio+iHpo8gZA0FCfdGUMjF0A\n".
+        "i+LmNR0dAkEAyLXJZNbmN6wyTwzT+838guo8W+6/CiZ6rfCqk4Sooqibph6k5QZu\n".
+        "g84mMJdm4F8zIZGNvqtKVUwGk149DZXRqwJAXrqGsxiGb6vYgWHmzsk/D1saYe50\n".
+        "ckxusB4rEzVaegp5jejSb0v0QUB/JnO9fKJnwXgs/l+psZSPzBTqDFiqeQJAGMK8\n".
+        "brFIQ3P93NyzRiw6S5hEC/9fGAx5M/4tvPcvqqlsUkkThKGPfrku4u26plF4SFrh\n".
+        "hrUw/WbcpM+KbqOd8wJAeOJbi6H9y9VonyQYJM7yXwhNeAvlKTYEyYPeW2O7oitg\n".
+        "1Nxmog30epbOchoAmCAr2TPzbpentnvCO1hKbO3Jkw==\n".
+        "-----END RSA PRIVATE KEY-----"
+        if (VPOSSend($array_send,$arrayOut,$llaveVPOSCryptoPub,$llavePrivadaFirmaComercio,$VI)) {
+        }else{
+            $msj = "Hay un problema con el conector de pago"; //puede haber un problema de mala configuraciÃ³n de las llaves, vector de
+            //inicializacion o el VPOS no ha enviado valores correctos
+        }
+        return $idorden;
     // END
 }
 /* End of file pi.vpost.php */
