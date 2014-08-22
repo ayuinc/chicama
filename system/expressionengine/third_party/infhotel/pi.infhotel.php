b588994c112a8c810806cba45799fc3f35d0c19a
Gianfranco Montoya <gms122@gmail.com>
2014-08-22 15:41:35 -0500

    logs send_json

--- system/expressionengine/third_party/infhotel/pi.infhotel.php
+++ system/expressionengine/third_party/infhotel/pi.infhotel.php
@@ -509,7 +509,7 @@ class Infhotel
         $data = json_decode($json, true);
         $data["Pasajeros"]["0"]= $person;
         //return $json;
-        $data["NPasajero"]="10";
+        $data["NPasajeros"]="10";
         
         $data_string = json_encode($data, true);
         $url = 'http://190.41.151.102/Infhotel/ServiceReservaWeb.svc/InsertReserva';
