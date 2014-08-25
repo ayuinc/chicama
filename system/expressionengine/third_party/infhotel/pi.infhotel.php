fe34c51ad5d0bde66d3236cf33fe4f023a8385ef
Gianfranco Montoya <gms122@gmail.com>
2014-08-25 16:05:18 -0500

    probando reserva

--- system/expressionengine/third_party/infhotel/pi.infhotel.php
+++ system/expressionengine/third_party/infhotel/pi.infhotel.php
@@ -507,13 +507,13 @@ class Infhotel
         $json = str_replace("(f)", ",", $json);
         
         $data = json_decode($json, true);
-
         $data["Pasajeros"]["0"]= $person;
+        var_dump($data);
         //return $json;
         //$data["NPasajeros"]="10";
 
         $data_string = json_encode($data, true);
-        var_dump($data_string);
+        //var_dump($data_string);
         $url = 'http://190.41.151.102/Infhotel/ServiceReservaWeb.svc/InsertReserva';
         //  Initiate curl
         $ch = curl_init($url);
