6908805f311aa6c0adf383293190e0445f260f0d
Gianfranco Montoya <gms122@gmail.com>
2014-09-24 12:43:31 -0500

    add select options in addons

--- system/expressionengine/third_party/infhotel/pi.infhotel.php
+++ system/expressionengine/third_party/infhotel/pi.infhotel.php
@@ -775,6 +775,15 @@ class Infhotel
                                     <option value="1">1</option>
                                     <option value="2">2</option>
                                     <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                    <option value="11">11</option>
+                                    <option value="12">12</option>
                                   </select>
                                 </p>
                               </div>
@@ -818,6 +827,15 @@ Note: If you don’t choose to Include meals, then you have the option to choose
                                     <option value="1">1</option>
                                     <option value="2">2</option>
                                     <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                    <option value="11">11</option>
+                                    <option value="12">12</option>
                                   </select>
                                 </p>
                               </div>
@@ -860,6 +878,15 @@ Note: If you don’t choose to Include meals, then you have the option to choose
                                     <option value="1">1</option>
                                     <option value="2">2</option>
                                     <option value="3">3</option>
+                                    <option value="4">4</option>
+                                    <option value="5">5</option>
+                                    <option value="6">6</option>
+                                    <option value="7">7</option>
+                                    <option value="8">8</option>
+                                    <option value="9">9</option>
+                                    <option value="10">10</option>
+                                    <option value="11">11</option>
+                                    <option value="12">12</option>
                                   </select>
                                 </p>
                               </div>
