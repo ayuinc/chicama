72f71d1431a42e613ce4479c90e859d60cba6f19
Gianfranco Montoya <gms122@gmail.com>
2014-10-06 16:01:29 -0500

    cost per night

--- system/expressionengine/third_party/infhotel/pi.infhotel.php
+++ system/expressionengine/third_party/infhotel/pi.infhotel.php
@@ -990,6 +990,7 @@ take about 1 hour. Our professional drivers carry with them a sign that will hav
                           <hr>
 
                           <p id="total_summary_per_night" class="ys-label"></p>
+                          <p id="dias_num" class="ys-label"></p>
                           <p id="total_summary" class="ys-label"></p>
                           <p id="total_summary_per_person" class="ys-label"></p>
                           <p id="total_summary_per_room" class="ys-label"></p>
