8cc080555d35a782650a8c2d2cccf51fbebe2432
Gianfranco Montoya <gms122@gmail.com>
2014-12-11 14:30:42 -0500

    agregando dias en summary

--- system/expressionengine/third_party/infhotel/pi.infhotel.php
+++ system/expressionengine/third_party/infhotel/pi.infhotel.php
@@ -800,12 +800,12 @@ class Infhotel
                                     <option value="4">4</option>
                                     <option value="5">5</option>
                                     <option value="6">6</option>
-                                    <option value="7">7</option>
+                                    <!-- <option value="7">7</option>
                                     <option value="8">8</option>
                                     <option value="9">9</option>
                                     <option value="10">10</option>
                                     <option value="11">11</option>
-                                    <option value="12">12</option>
+                                    <option value="12">12</option> -->
                                   </select>
                                 </p>
                               </div>
@@ -852,12 +852,12 @@ Note: If you don’t choose to Include meals, then you have the option to choose
                                     <option value="4">4</option>
                                     <option value="5">5</option>
                                     <option value="6">6</option>
-                                    <option value="7">7</option>
+                                    <!-- <option value="7">7</option>
                                     <option value="8">8</option>
                                     <option value="9">9</option>
                                     <option value="10">10</option>
                                     <option value="11">11</option>
-                                    <option value="12">12</option>
+                                    <option value="12">12</option> -->
                                   </select>
                                 </p>
                               </div>
@@ -903,12 +903,12 @@ Note: If you don’t choose to Include meals, then you have the option to choose
                                     <option value="4">4</option>
                                     <option value="5">5</option>
                                     <option value="6">6</option>
-                                    <option value="7">7</option>
+                                    <!-- <option value="7">7</option>
                                     <option value="8">8</option>
                                     <option value="9">9</option>
                                     <option value="10">10</option>
                                     <option value="11">11</option>
-                                    <option value="12">12</option>
+                                    <option value="12">12</option> -->
                                   </select>
                                 </p>
                               </div>
