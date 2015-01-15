07ff2d5e96e8271bc7a163c65110a2c2b2fb4807
Gianfranco Montoya <gms122@gmail.com>
2015-01-15 11:15:47 -0500

    ingresando condicional de pago

--- system/expressionengine/third_party/mandrillapp/pi.mandrillapp.php
+++ system/expressionengine/third_party/mandrillapp/pi.mandrillapp.php
@@ -45,6 +45,7 @@ class Mandrillapp {
 	$add_on_1_detail = "";
 	$add_on_2_detail = "";
 	$add_on_3_detail = "";
+	$add_on_3_extra_amount = 0;
 	ee()->db->select('*');
 	ee()->db->where('id',$id);
 	$query = ee()->db->get('exp_hotel_reservations');
